0ac9020bd0b55225abc12d88c8193947283df8ac
patricia <patricia.bonaldy@lana.xyz>
2022-06-14 13:08:35 +0100

    add config file

---
+++ internal/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	_ "embed"
+	"encoding/json"
+
+	"github.com/pkg/errors"
+)
+
+type Database struct {
+	DatabaseName string `json:"db_name"`
+	User         string `json:"db_user"`
+	Password     string `json:"db_pass"`
+	URI          string `json:"db_dsn"`
+}
+
+type Kafka struct {
+	Broker string `json:"broker"`
+	Topic  string `json:"topic"`
+}
+
+type Config struct {
+	Host            string    `json:"host"`
+	Port            int       `json:"port"`
+	SportsNewsURL   int       `json:"sports_news_url"`
+	ShutdownTimeout int       `json:"shutdown_timeout"`
+	Database        *Database `json:"database"`
+	Kafka           *Kafka    `json:"kafka"`
+}
+
+//go:embed config.json
+var data []byte
+
+// New returns a new configuration, and attempts to load
+// config from file system.
+func New() (*Config, error) {
+	cfg := &Config{}
+	err := json.Unmarshal(data, cfg)
+	if err != nil {
+		return nil, errors.Errorf("couldn't parse json file.: %s", err)
+	}
+
+	return cfg, nil
+}
